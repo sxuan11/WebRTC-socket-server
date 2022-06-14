a1d927d98533db047530360086cebf74b96fadc9
sxuan <609311490@qq.com>
2022-06-14 23:51:28 +0800

    feat 新增后进入成员的广播

--- io/index.js
+++ io/index.js
@@ -1,45 +1,87 @@
+const { debounce }  = require('lodash');
+
+
 module.exports = async function (fastify, opts) {
   const redis = fastify.redis;
-  const userList = new Map();
-
-  function broadcast() {
-
-  }
-
-  function setUser(userId, nick) {
-    userList.set(userId, { nick })
-  }
-
-  function rmUser(userId) {
-    userList.delete(userId);
-  }
-  fastify.io.on('connection', (socket) => {
-    console.log('some one join')
+  const IONameSpace = fastify.io.of('/');
+  fastify.io.on('connection', async (socket) => {
     const room = socket.handshake.query.room;
-    const roomUser = redis.get(room);
-    if(!roomUser) {
-      redis.set(room, userList)
+    const roomUser = await redis.get(room);
+    let userList;
+    if(roomUser) {
+      userList = new Map(JSON.parse(roomUser))
+    } else {
+      userList = new Map();
     }
+    function broadcastToNewLogin(id) {
+      for (const [k, v] of userList) {
+        if(Object.prototype.hasOwnProperty.call(v, 'offer')) {
+          IONameSpace.sockets.get(id).emit('offer', v.offer);
+        }
+        if(Object.prototype.hasOwnProperty.call(v, 'candidate')) {
+          for (const vElement of v.candidate) {
+            IONameSpace.sockets.get(id).emit('ICE-candidate', vElement);
+          }
+        }
+      }
+    }
+
+    function setUser(userId, type, value) {
+      if(userList.get(userId)) {
+        userList.set(userId, { ...userList.get(userId), [type]: value })
+      } else {
+        userList.set(userId, { [type]: value })
+      }
+    }
+
+    function setICE(userId, data) {
+      const obj = userList.get(userId)
+      if(obj.candidate) {
+        obj.candidate = [...obj.candidate].concat([data]);
+      } else {
+        obj.candidate = [data]
+      }
+      userList.set(userId, obj);
+    }
+
+    function rmUser(userId) {
+      userList.delete(userId);
+    }
+
+    async function setRedisRoom() {
+      await redis.set(room, JSON.stringify([...userList]))
+    }
+
+    const debounceSetRedis = debounce(setRedisRoom, 1000)
     const userId = socket.handshake.query.userId || Math.random();
     const nick = socket.handshake.query.nick || Math.random();
-    setUser(userId ,nick)
+    setUser(userId ,'nick', nick);
+    setUser(userId ,'sockId', socket.id);
     if (room) {
       socket.join(room);
       socket.to(room).emit('join', userId);
+      await debounceSetRedis();
+      broadcastToNewLogin(socket.id)
     }
-    socket.on('disconnect', (socket) => {
+    socket.on('disconnect', async (socket) => {
       console.log('someone leave->', socket)
+      rmUser(userId);
+      await debounceSetRedis();
     })
 
     socket.on('speak', (message, callback) => {
       socket.to(room).emit('speak', message);
+      console.log(IONameSpace.sockets, 'sockets');
       callback({
         status: "ok"
       })
     })
 
-    socket.on('offer', (offer, callback) => {
+    socket.on('offer', async (offer, callback) => {
       socket.to(room).emit('offer', offer);
+      setUser(userId, 'offer', offer);
+      console.log("=>(index.js:67) offer", offer, userId);
+      await debounceSetRedis();
       callback({
         status: "ok"
       })
@@ -52,8 +94,10 @@ module.exports = async function (fastify, opts) {
       })
     })
 
-    socket.on('ICE-candidate', (data, callback) => {
+    socket.on('ICE-candidate', async (data, callback) => {
       socket.to(room).emit('ICE-candidate', data);
+      setICE(userId, data);
+      await debounceSetRedis();
       callback({
         status: "ok"
       })
